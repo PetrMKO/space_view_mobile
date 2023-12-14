1ef75cc785bf028ce51c45a847ec3b9be8e4b902
semen.mokrov <semen.mokrov@gmail.com>
2023-12-15 01:20:52 +0300

    feat: add redirect after log in

---
+++ src/types/AuthMode.ts
@@ -0,0 +1,17 @@
+type AuthMode = "login" | "signin";
+
+export enum Screens {
+  Main = "Main",
+  AuthSelector = "AuthSelector",
+  Auth = "Auth",
+  Account = "Account",
+}
+
+export type RootStackParamList = {
+  [Screens.Main]: undefined;
+  [Screens.Account]: undefined;
+  [Screens.Auth]: {
+    mode: AuthMode;
+  };
+  [Screens.Main]: undefined;
+};
